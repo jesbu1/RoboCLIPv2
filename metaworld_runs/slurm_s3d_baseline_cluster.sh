cb5db180f50e72256150cd1a1cc856463e67efdd
Jiahui Zhang <jzhang96@usc.edu>
2024-07-20 12:28:58 -0700

    update titan

--- metaworld_runs/slurm_s3d_baseline_cluster.sh
+++ metaworld_runs/slurm_s3d_baseline_cluster.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name=sac_s3d_baselines       # Job name
 #SBATCH --output=/home/jzhang96/metaworld_log/s3d_transform.out   # Output file
 #SBATCH --error=/home/jzhang96/metaworld_log/s3d_transform.err    # Error file
-#SBATCH --ntasks=3                      # Number of tasks (processes)
+#SBATCH --ntasks=1                      # Number of tasks (processes)
 #SBATCH --gres=gpu:6000:1                        # Number of GPUs
 #SBATCH --cpus-per-task=10               # Number of CPU cores per task
-#SBTCH --nodelist ink-titan
+#SBATCH --nodes=1
+#SBTCH --nodelist=ink-titan
 #SBATCH --time=24:00:00                  # Time limit hrs:min:sec
 
 
@@ -19,9 +20,9 @@ conda deactivate
 conda activate roboclip
 which python
 
-PYTHON_SCRIPT_1="python metaworld_envs_s3d_baseline.py --n_envs 8 --wandb --time --train_orcale --norm_input --norm_output --time_100 --entropy_term 0.5 --random_reset --eval_freq 1280 --video_freq 5120 --seed 42" 
-PYTHON_SCRIPT_2="python metaworld_envs_s3d_baseline.py --n_envs 8 --wandb --time --train_orcale --norm_input --norm_output --time_100 --entropy_term 0.5 --random_reset --eval_freq 1280 --video_freq 5120 --seed 5" 
-PYTHON_SCRIPT_3="python metaworld_envs_s3d_baseline.py --n_envs 8 --wandb --time --train_orcale --norm_input --norm_output --time_100 --entropy_term 0.5 --random_reset --eval_freq 1280 --video_freq 5120 --seed 32" 
+PYTHON_SCRIPT_1="python metaworld_envs_s3d_baseline.py --n_envs 8 --wandb --time --train_orcale --norm_input --norm_output --time_100 --entropy_term 0.5 --random_reset --eval_freq 1280 --video_freq 5120 --seed 42 --time_penalty 0.1 --succ_end"
+# PYTHON_SCRIPT_2="python metaworld_envs_s3d_baseline.py --n_envs 8 --wandb --time --train_orcale --norm_input --norm_output --time_100 --entropy_term 0.5 --random_reset --eval_freq 1280 --video_freq 5120 --seed 5 --time_penalty 0.1 --succ_end"
+# PYTHON_SCRIPT_3="python metaworld_envs_s3d_baseline.py --n_envs 8 --wandb --time --train_orcale --norm_input --norm_output --time_100 --entropy_term 0.5 --random_reset --eval_freq 1280 --video_freq 5120 --seed 32 --time_penalty 0.1 --succ_end" 
 
 
 
