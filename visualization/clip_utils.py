0e38bb5c62a64f70979da8f048b21ea23b248d1d
AndreasL9z <yusenluo@usc.edu>
2024-10-16 09:10:49 +0000

    subtract

---
+++ visualization/clip_utils.py
@@ -0,0 +1,215 @@
+import clip
+import torch
+import imageio
+import numpy as np
+from PIL import Image
+from liv import load_liv
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torchvision.transforms as T
+from transformers import CLIPProcessor, CLIPModel, AutoTokenizer, AutoProcessor
+from tqdm import tqdm
+from sklearn.decomposition import PCA
+import json
+import os
+import joblib
+
+
+
+
+
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
+def load_model(model_name):
+    if model_name == "clip":
+        model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+        processor = AutoProcessor.from_pretrained("openai/clip-vit-large-patch14")
+        tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+    elif model_name == "liv":
+        model = load_liv()
+        model.eval()
+        processor = None
+        tokenizer = None
+    else:
+        raise ValueError(f"Model {model_name} not supported")
+    return model, processor, tokenizer
+
+def embedding_text(model, tokenizer, text):
+    if type(text) != list:
+        text = [text]
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if tokenizer is not None:
+        inputs = tokenizer(text=text, return_tensors="pt", padding=True)
+        model = model.to(device)
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+        with torch.no_grad():
+            text_embeddings = model.get_text_features(**inputs)
+        text_embeddings = text_embeddings / text_embeddings.norm(dim=-1, keepdim=True)
+    else:
+        text = clip.tokenize(text)
+        text_embeddings = model(input=text, modality="text")
+    return text_embeddings
+
+def embedding_image(model, processor, image):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if type(image) != Image.Image:
+        image = Image.fromarray(image.astype(np.uint8))
+    if processor is not None:
+        inputs = processor(images=image, return_tensors="pt", padding=True).to(device)
+        with torch.no_grad():
+            image_embeddings = model.get_image_features(**inputs)
+        image_embeddings = image_embeddings / image_embeddings.norm(dim=-1, keepdim=True)
+    else:
+        image = T.ToTensor()(image).unsqueeze(0).to(device)
+        with torch.no_grad():
+            image_embeddings = model(input=image, modality="vision")
+    return image_embeddings
+
+def compute_similarity(text_embeddings, image_embeddings):
+    cosine_similarity = F.cosine_similarity(text_embeddings, image_embeddings)
+    return cosine_similarity
+
+def normalize_embeddings(embeddings, return_tensor=True):
+    if isinstance(embeddings, np.ndarray):
+        embeddings = torch.tensor(embeddings)
+    normalized_embeddings = F.normalize(embeddings, p=2, dim=1)
+    if return_tensor:
+        return normalized_embeddings.cpu()
+    else:
+        return normalized_embeddings.detach().cpu().numpy()
+
+class SingleLayerMLP(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SingleLayerMLP, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        x = self.linear(x)
+        return x
+
+class TwoLayerMLP(torch.nn.Module):
+    def __init__(self, input_dim):
+        super(TwoLayerMLP, self).__init__()
+        self.linear1 = torch.nn.Linear(input_dim, input_dim // 2)
+        self.linear2 = torch.nn.Linear(input_dim // 2, 1)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        x = F.tanh(x)
+        return x
+
+class ThreeLayerMLP(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(ThreeLayerMLP, self).__init__()
+        self.linear1 = torch.nn.Linear(input_dim, input_dim)
+        self.linear2 = torch.nn.Linear(input_dim, output_dim)
+        self.linear3 = torch.nn.Linear(output_dim, output_dim)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        x = F.normalize(x, p=2, dim=1)
+        return x
+
+
+def pca_learner(h5_file, model_name, only_goal_image = True, pca_var = 0.95):
+
+    folder_path = "pca_models"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
+
+    text_file_name = f"{folder_path}/{model_name}_text_pca_model_var" + str(pca_var)
+    image_file_name = f"{folder_path}/{model_name}_image_pca_model_var" + str(pca_var)
+
+    if only_goal_image:
+        text_file_name = text_file_name + "_goal"
+        image_file_name = image_file_name + "_goal"
+
+    text_file_name = text_file_name + ".pkl"
+    image_file_name = image_file_name + ".pkl"
+
+
+    if os.path.exists(text_file_name) and os.path.exists(image_file_name):
+        text_pca_model = joblib.load(text_file_name)
+        image_pca_model = joblib.load(image_file_name)
+
+    else:
+        train_envs = json.load(open("task_subset.json"))["subset_6"]
+        text_embeddings = []
+        image_embeddings = []
+        for env in train_envs:
+            text_env_name = f"{env}_text"
+            text_array = h5_file[model_name][text_env_name][:]
+            text_embeddings.append(np.array(text_array))
+
+            image_dataset = h5_file[model_name][env]
+
+            for key in sorted(image_dataset.keys(), key = int)[:15]:
+                image_array = image_dataset[key][:]
+                if only_goal_image:
+                    image_array = image_array[-1:]
+                image_embeddings.append(np.array(image_array))
+
+        text_embeddings = np.concatenate(text_embeddings, axis=0)
+        image_embeddings = np.concatenate(image_embeddings, axis=0)
+        text_embeddings = normalize_embeddings(torch.from_numpy(text_embeddings).to(device))
+        image_embeddings = normalize_embeddings(torch.from_numpy(image_embeddings).to(device))
+        print("text_embeddings shape", text_embeddings.shape)
+        print("image_embeddings shape", image_embeddings.shape)
+        
+        if pca_var < 1:
+            text_pca_model = PCA(n_components = pca_var)
+        else:
+            text_pca_model = PCA(n_components = text_embeddings.shape[0])
+        text_pca_model.fit(text_embeddings.cpu())
+        print("Text PCA Model Fitted", text_pca_model.n_components_)
+        joblib.dump(text_pca_model, text_file_name)
+        
+        
+
+        image_pca_model = PCA(n_components = text_pca_model.n_components_)
+        image_pca_model.fit(image_embeddings.cpu())
+        print("Image PCA Model Fitted", image_pca_model.n_components_)
+        joblib.dump(image_pca_model, image_file_name)
+
+    return text_pca_model, image_pca_model
+
+def compute_M(X_S, X_T):
+    M = np.dot(X_S, X_T.T)  # 35 35
+    M_tensor = torch.from_numpy(M).float()
+
+    return M_tensor
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    model_name = "clip"
+    # model_name = "liv"
+    model, processor, tokenizer = load_model(model_name)
+    text = "Robot closing a door"
+    text_embeddings = embedding_text(model, tokenizer, text)
+
+    image = np.random.randint(0, 255, (224, 224, 3)).astype(np.uint8)
+    image = Image.fromarray(image)
+    image_embeddings = embedding_image(model, processor, image)
+
+    similarity = compute_similarity(text_embeddings, image_embeddings)
+    print(similarity)
+    # hardnesses = ["all_fail", "close_fail", "success", "GT"]
+
+    
---
+++ visualization/regression_training.py
@@ -0,0 +1,212 @@
+import torch
+from PIL import Image
+from dataloader_clipliv import ClipLivDataset, ClipLivProgressDataset, ClipLivSingleDataset
+import torch.nn.functional as F
+import numpy as np
+import random
+from torch.utils.data import DataLoader
+import argparse
+from clip_utils import normalize_embeddings, compute_similarity, TwoLayerMLP, pca_learner, compute_M, SingleLayerMLP
+from torch.nn import MarginRankingLoss
+import wandb
+from tqdm import tqdm
+import h5py
+from eval_rank_utils import plot_progress, plot_progress_corr, plot_videos
+from torch.nn.functional import mse_loss
+
+
+def AdaptiveMarginTripletLoss(positive_score, negative_score, margin_range, progress):
+    margin = margin_range * (1 - progress)
+    loss = F.relu(margin + negative_score - positive_score)
+    return loss.mean()
+
+
+
+
+
+
+
+
+def main(args):
+    
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    WANDB_ENTITY_NAME = "clvr"
+    WANDB_PROJECT_NAME = "roboclip-v2"
+    experiment_name = "RegressionLoss_" + args.model_name + "_" + args.loss_type
+
+    if args.pca:
+        experiment_name += "_pca_" + str(args.pca_var)
+        if args.pca_only_goal:
+            experiment_name += "_only_goal"
+
+    if args.loss_type == "triplet":
+        experiment_name += "_margin_" + str(args.margin_range)
+    
+    if args.Subtract:
+        experiment_name += "_Subtract"
+
+
+    run = wandb.init(
+        entity=WANDB_ENTITY_NAME,
+        project=WANDB_PROJECT_NAME,
+        group="Rank_loss_train_update",
+        config=args,
+        name=experiment_name,
+    )
+
+
+
+
+    h5_file = h5py.File(args.h5_embedding_path, "r")
+    text_pca_model, image_pca_model, linear_model = None, None, None
+    if args.pca:
+        text_pca_model, image_pca_model = pca_learner(h5_file, args.model_name, args.pca_only_goal, args.pca_var)
+        computed_matrix = compute_M(image_pca_model.components_, text_pca_model.components_)
+        linear_model = SingleLayerMLP(image_pca_model.components_.shape[0], text_pca_model.components_.shape[0]).to(device)
+        linear_model.linear.weight.data = computed_matrix.to(device).float()
+        linear_model.linear.requires_grad = False
+
+
+
+
+
+    dataset = ClipLivSingleDataset(args, h5_file)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=0, drop_last=True)
+
+    if args.loss_type == "triplet":
+        loss_function = AdaptiveMarginTripletLoss
+    elif args.loss_type == "mse":
+        loss_function = mse_loss
+    else:
+        raise ValueError("Invalid loss type")
+    if args.pca:
+        transform_model = TwoLayerMLP(image_pca_model.components_.shape[0] + text_pca_model.components_.shape[0]).to(device)
+    else:
+        if args.model_name == "clip":
+            transform_model = TwoLayerMLP(768 * (2 - args.Subtract)).to(device)
+        elif args.model_name == "liv":
+            transform_model = TwoLayerMLP(1024 * (2 - args.Subtract)).to(device)
+
+
+    optimizer = torch.optim.Adam(transform_model.parameters(), lr=args.lr)
+
+
+    corr_train_dict = plot_progress_corr(h5_file, args.model_name, transform_model, "train", text_pca_model, image_pca_model, linear_model)
+    corr_eval_dict = plot_progress_corr(h5_file, args.model_name, transform_model, "eval", text_pca_model, image_pca_model, linear_model)
+    plot_progress(h5_file, args.model_name, transform_model, "train", text_pca_model, image_pca_model, linear_model, args.Subtract)
+    plot_progress(h5_file, args.model_name, transform_model, "eval", text_pca_model, image_pca_model, linear_model, args.Subtract)
+    plot_videos(args.model_name, transform_model, text_pca_model, image_pca_model, linear_model, args.Subtract)
+
+    for epoch in range(args.epochs):
+        transform_model.train()
+        # total_loss = 0
+        for i, data in enumerate(tqdm(dataloader)):
+            text_array = normalize_embeddings(data["text_array"].to(device)).float()
+            progress_array = normalize_embeddings(data["video_array"].to(device)).float()
+            progress = data["progress"].to(device).float().unsqueeze(1)
+            if args.loss_type == "triplet":
+                goal_array = normalize_embeddings(data["goal_array"].to(device)).float()
+            gt_score = None
+            if args.pca:
+                
+                text_array = text_pca_model.transform(text_array.detach().cpu().numpy())
+                text_array = torch.tensor(text_array).to(device).float()
+                with torch.no_grad():
+                    progress_array = image_pca_model.transform(progress_array.detach().cpu().numpy())
+                    progress_array = torch.tensor(progress_array).to(device).float()
+
+                    progress_array = linear_model(progress_array)
+
+                
+                if args.loss_type == "triplet":
+                    with torch.no_grad():
+                        goal_array = image_pca_model.transform(goal_array.detach().cpu().numpy())
+                        goal_array = torch.tensor(goal_array).to(device).float()
+                        goal_array = linear_model(goal_array)
+
+
+            feature_dim = text_array.shape[1]
+
+            
+
+
+
+            # process progress feature
+            if args.Subtract:
+                progress_input = progress_array - text_array
+            else:
+                progress_input = torch.cat([text_array, progress_array], dim=1)
+            progress_output = transform_model(progress_input)
+
+            # process goal feature
+            if args.loss_type == "triplet":
+                goal_input = torch.cat([text_array, goal_array], dim=1)
+                goal_output = transform_model(goal_input)
+                gt_score = goal_output.mean(dim=0).item()
+
+            # compute loss
+
+            if args.loss_type == "triplet":
+                loss = loss_function(goal_output, progress_output, args.margin_range, progress)
+            elif args.loss_type == "mse":
+                loss = loss_function(progress_output, progress)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            wandb_log = {
+                "loss": loss.item(),
+                "feature_dim": feature_dim
+            }
+            if gt_score is not None:
+                wandb_log["gt_score"] = gt_score
+            wandb.log(wandb_log)
+
+
+        if epoch % 10 == 0:
+            corr_train_dict = plot_progress_corr(h5_file, args.model_name, transform_model, "train", text_pca_model, image_pca_model, linear_model)
+            corr_eval_dict = plot_progress_corr(h5_file, args.model_name, transform_model, "eval", text_pca_model, image_pca_model, linear_model)
+            plot_progress(h5_file, args.model_name, transform_model, "train", text_pca_model, image_pca_model, linear_model, args.Subtract)
+            plot_progress(h5_file, args.model_name, transform_model, "eval", text_pca_model, image_pca_model, linear_model, args.Subtract)
+            plot_videos(args.model_name, transform_model, text_pca_model, image_pca_model, linear_model, args.Subtract)
+
+            wandb_progress_log = {}
+            wandb_progress_log.update(corr_train_dict)
+            wandb_progress_log.update(corr_eval_dict)
+            wandb.log(wandb_progress_log)
+
+        
+
+
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--h5_embedding_path', type=str, default='/scr/jzhang96/metaworld_25_for_clip_liv.h5')
+    argparser.add_argument('--model_name', type=str, default='clip')
+    argparser.add_argument('--batch_size', type=int, default=32)
+    argparser.add_argument('--epochs', type=int, default=500)
+    argparser.add_argument('--seed', type=int, default=42)
+    argparser.add_argument('--lr', type=float, default=1e-3)
+    argparser.add_argument('--loss_type', type=str, choices=['triplet', 'mse'], default='mse')
+    argparser.add_argument('--margin_range', type=float, default=1.0)
+    argparser.add_argument('--pca', action='store_true')
+    argparser.add_argument('--pca_only_goal', action='store_true')
+    argparser.add_argument('--Subtract', action='store_true')
+    argparser.add_argument('--pca_var', type=float, default=1.0)
+    args = argparser.parse_args()
+    main(args)
+
+
+
+
+
+
+    
