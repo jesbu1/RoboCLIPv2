f8388b0444e5934a8bc1038b8b9b16029774f993
Jiahui-3205 <jzhang96@usc.edu>
2025-01-13 05:46:52 +0000

    update confusion matrix

--- liv_train_final/confusion_matrix_decoder.py
+++ liv_train_final/confusion_matrix_decoder.py
@@ -56,61 +56,6 @@ def plot_matrix_as_image(matrix, names, set, text):
 
 
 
-def plot_confusion_matrix_pca(h5_file, model_name, set, self_attention_model, args):
-    device = next(self_attention_model.parameters()).device
-    model, processor, tokenizer = load_model(model_name)
-    if set == "train":
-        eval_envs = json.load(open("task_subset.json"))["subset_6"]
-        text = json.load(open("task_subset.json"))["train_annotation"]
-    elif set == "eval":
-        eval_envs = json.load(open("task_subset.json"))["evaluate_tasks"]
-        text = json.load(open("task_subset.json"))["eval_annotation"]
-    text_embeddings = embedding_text(model, tokenizer, text).to(device).float()
-    if args.normalize_embedding:
-        text_embeddings = normalize_embeddings(text_embeddings)
-
-
-
-    predicted_progress_row = []
-    for i  in tqdm(range(len(eval_envs))):
-        env = eval_envs[i]
-        
-        model_group = h5_file[model_name]
-        env_group = model_group[env]
-        traj_idx = list(env_group.keys())
-        idx = random.randint(0, len(traj_idx)-1)
-        traj_key = traj_idx[idx]
-        traj_data = np.asarray(env_group[traj_key])
-
-        traj_data = torch.tensor(traj_data).to(device).float()
-        if args.subsample_video:
-            traj_data = padding_video(traj_data, args.max_length)
-
-        if args.normalize_embedding:
-            traj_data = normalize_embeddings(traj_data)
-        if pca_video_model is not None:
-            traj_data = traj_data.view(-1, 1024)
-            traj_data = pca_video_model.transform(traj_data.cpu().detach().numpy())
-            traj_data = torch.tensor(traj_data).to(device).float()
-            traj_data = transform_model(traj_data)
-
-
-        # for i in range(traj_data.shape[0]):
-
-        video_frame_data = traj_data[:]
-        video_frame_data = video_frame_data.unsqueeze(0)
-        video_frame_data = video_frame_data.repeat(text_embeddings.shape[0], 1, 1)
-
-        predicted_score, two_step_class = self_attention_model(video_frame_data, mask=None, text_array=text_embeddings)
-        if args.two_step_training:
-            pred_two_class = torch.argmax(two_step_class, dim = 1).unsqueeze(1)
-            predicted_score = pred_two_class * predicted_score
-
-        predicted_progress = np.array(predicted_score.squeeze().detach().cpu().numpy())
-        predicted_progress_row.append(predicted_progress)
-    predicted_progress_row = np.array(predicted_progress_row)
-
-    img = plot_matrix_as_image(predicted_progress_row, eval_envs, set, text)
 
 
 
