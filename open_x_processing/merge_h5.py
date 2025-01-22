7ce010b810f5b1456d9cf62e00b73ac7f56c67bd
Jesse Zhang <jessezhang5@gmail.com>
2025-01-21 23:58:09 -0800

    correct h5 path

--- open_x_processing/merge_h5.py
+++ open_x_processing/merge_h5.py
@@ -14,7 +14,7 @@ from clip_utils import (
 )
 from PIL import Image
 
-MAIN_H5 = "/data/shared/roboclip/data/h5_buffers/openx_embeddings/openx_embeddings_full_uncompressed_processed.h5"
+MAIN_H5 = "/data/shared/roboclip/data/h5_buffers/openx_embeddings/openx_embeddings_full_uncompressed_with_langtable.h5"
 H5_MERGING_FROM = "openx_embeddings_lang_table.h5"
 print(f"Adding to {MAIN_H5} from {H5_MERGING_FROM}")
 
