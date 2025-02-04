4ca328a47a0eec46352b4f731feaf5672efac3b7
jiahui <jzhang96@usc.edu>
2025-02-04 11:49:22 -0800

    update

--- real_robot_exp/eval_confusion_matrix.py
+++ real_robot_exp/eval_confusion_matrix.py
@@ -113,6 +113,8 @@ def plot_confusion_matrix(h5_file, set, self_attention_model, args, prob = False
             eval_envs = keys
         else:
             eval_envs = keys[int(len(keys)*0.75):]
+    else:
+        eval_envs = keys
 
     text_embeddings = []
     text_list = []
