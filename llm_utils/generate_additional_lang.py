ee65ffd26c066ff885df886125be584346263232
Jesse Zhang <jessez@usc.edu>
2025-02-06 15:24:40 -0800

    updated to use Mistral7B

--- llm_utils/generate_additional_lang.py
+++ llm_utils/generate_additional_lang.py
@@ -5,7 +5,9 @@ import json
 from transformers import pipeline
 import os 
 huggingface_token = os.environ.get("HF_TOKEN", None)  
-assert huggingface_token is not None, "Please set the HF_TOKEN environment variable to your Hugging Face API token and make sure you were granted access to https://huggingface.co/google/gemma-2-2b-it"
+#pip install sentencepiece for mistral
+#assert huggingface_token is not None, "Please set the HF_TOKEN environment variable to your Hugging Face API token and make sure you were granted access to https://huggingface.co/google/gemma-2-2b-it"
+assert huggingface_token is not None, "Please set the HF_TOKEN environment variable to your Hugging Face API token and make sure you were granted access to https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.3"
 
 h5_path = "usc_koch_rewind_reward.h5"
 
@@ -24,20 +26,20 @@ print(lang_instructions)
 
 pipe = pipeline(
     "text-generation",
-    model="google/gemma-2-2b-it",
-    #model="mistralai/Mistral-7B-Instruct-v0.3",
+    #model="google/gemma-2-2b-it",
+    model="mistralai/Mistral-7B-Instruct-v0.3",
     #model="meta-llama/Llama-3.2-3B-Instruct",
     #model="Qwen/Qwen2.5-VL-3B-Instruct",
     torch_dtype=torch.bfloat16,
     device="cuda",  # replace with "mps" to run on a Mac device
     #use_auth_token=True,
     token=huggingface_token,
-    do_sample=False,
+    do_sample=True,
 )
 
 # now, we can generate additional language instructions using the model
 for lang_key in lang_instructions:
-    while len(lang_instructions[lang_key]) < 5:
+    while len(lang_instructions[lang_key]) < 10:
         try:
             prompt = f"Please write out 5 additional instructions with the same meaning for the following instruction: {lang_key}. Output the answer in a python list like ```python\n['instr_1', 'instr_2', 'instr_3', 'instr_4']\n``` \n ```python\n"
             outputs = pipe(prompt, max_new_tokens=80)
@@ -55,7 +57,7 @@ for lang_key in lang_instructions:
             # now add unique instructions to the lang_instructions
             for instr in parsed_response:
                 # strip the last period
-                stripped_instr = instr.strip().strip(".")
+                stripped_instr = instr.strip().strip(".").capitalize()
                 if stripped_instr not in lang_instructions[lang_key]:
                     lang_instructions[lang_key].append(stripped_instr)
         except Exception as e:
