c01d34752f15452e39d7b949724d5e2fa81dc64e
henry <jzhang96@usc.edu>
2025-02-06 00:23:06 -0800

    update

---
+++ real_robot_exp/koch_key_name.py
@@ -0,0 +1,33 @@
+train_keys = [
+    'Move the orange cup from the left to the right', 
+    'Move the orange cup from the right to the left',
+
+    'Put the orange cup on the red plate',
+    'Put the red cup on the red plate',
+
+    'Separate the blue and red cups',
+
+    'Fold the blue towel',
+
+    'Open the green trash bin',
+    'Open the blue trash bin',
+
+    'Throw the banana away in the green trash bin',
+    'Throw the banana away in the blue trash bin',
+
+    'Put the red marker in the red trash can',
+    'Put the pink marker in the green trash can',
+
+    'Put the blue tape in the box on the left',
+
+    'Put the banana in the box',
+    'Put the orange cup in the box'
+]
+
+Eval_keys = [
+    'Put the blue cup on the red plate',
+    'Separate the orange and blue cups',
+    'Open the red trash bin',
+    'Throw the banana away in the red trash bin',
+    'Put the red tape in the box on the right'
+]
