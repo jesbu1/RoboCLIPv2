ddc1092db6aec9b08f716ba05ba1b2593644c871
Abrar Anwar <abraranwar123@gmail.com>
2025-04-23 13:38:16 -0700

    videos should work

--- offline_rl_algorithms/callbacks.py
+++ offline_rl_algorithms/callbacks.py
@@ -165,7 +165,9 @@ class OfflineEvalCallback(EvalCallback):
                     "Something went wrong when logging gradients/weights. Skipping logging"
                 )
 
-        if self.video_freq > 0 and self.n_calls % self.video_freq == 0:
+        if self.video_freq > 0 and (
+            self.n_calls % self.video_freq == 0 or self.n_calls == 1
+        ):
             video_buffer = self.record_video()
             # self.logger.record({f"evaluation_video": wandb.Video(video_buffer, fps=20, format="mp4")}, commit=False)
             self.logger.record(
@@ -191,11 +193,16 @@ class OfflineEvalCallback(EvalCallback):
             # downsample frame
             frame = frame[::3, ::3, :3]
             frames.append(frame)
-            action, _ = self.model.predict(
-                obs,
-                deterministic=True,
-                episode_start=np.array([first_step], env=self.eval_env),
-            )
+            try:
+                action, _ = self.model.predict(
+                    obs,
+                    deterministic=True,
+                    episode_start=np.array([first_step]),
+                    env=self.eval_env,
+                )
+            except Exception as e:
+                print("Exception in predict:", e)
+                breakpoint()
             obs, reward, done, info = self.eval_env.step(action)
             first_step = False
             if done:
