b524446a36f14de863aa991a1edcf6f3cb183693
Jiahui_citrine <jzhang96@usc.edu>
2025-04-23 04:25:24 -0700

    merge

---
+++ test_scripts/new_task_annotation_v2.py
@@ -0,0 +1,171 @@
+train_gt_annotation = {
+        "button-press-v2": "Press the button from side",
+        "button-press-topdown-wall-v2": "Press the button from top",
+        "coffee-pull-v2": "Pull the coffee cup",
+        "dial-turn-v2": "Turn the dial",
+        "door-open-v2": "Open the door",
+        "door-unlock-v2": "Turn door lock clockwise",
+        "drawer-close-v2": "Close the drawer",
+        "faucet-open-v2": "Open the faucet",
+        "handle-press-v2": "Press the handle",
+        "handle-pull-side-v2": "Pull the handle up from the side",
+        "peg-insert-side-v2": "Insert the peg",
+        "pick-place-v2": "Pick up the block and placing it to the goal position",
+        "plate-slide-v2": "Slide the plate into the gate",
+        "plate-slide-back-side-v2": "Slide the plate out of the gate from the side",
+        "push-v2": "Push the block to the goal",
+        "reach-v2": "Reach the goal",
+        "stick-push-v2": "Push the stick",
+        "stick-pull-v2": "Pull the stick",
+        "window-open-v2": "Open the window",
+        "hand-insert-v2": "Pick up the block and insert it into the hole",
+}
+
+eval_gt_annotation = {
+        "button-press-wall-v2": "Press the button from side",
+        "button-press-topdown-v2": "Press the button from top",
+        "coffee-push-v2": "Push the coffee cup",
+        "coffee-button-v2": "Press the coffee button",
+        "door-close-v2": "Close the door",
+        "door-lock-v2": "Turn door lock counter-clockwise",
+        # "drawer-open-v2": "Opening the drawer",
+        "faucet-close-v2": "Close the faucet",
+        "handle-press-side-v2": "Press the handle from side",
+        "handle-pull-v2": "Pull the handle",
+        # "peg-unplug-v2": "Pulling the peg",
+        "pick-place-wall-v2": "Pick up the block and placing it to the goal position",
+        "plate-slide-back-v2": "Slide the plate out of the gate",
+        "plate-slide-side-v2": "Slide the plate into the gate from the side",
+        "push-back-v2": "Push the block back to the goal",
+        "reach-wall-v2": "Reach the goal",
+        "soccer-v2": "Slide the ball into the gate",
+        "window-close-v2": "Close the window",
+        "sweep-into-v2": "Sweep the block into the hole"}
+
+generated_gt_annotation = {
+    "button-press-v2":[
+        "Press the button from the side",
+        "Press the red button",
+        "Press the button on the yellow box",
+        "Push the button inside"
+    ],
+    "button-press-topdown-wall-v2":[
+        "Press the button from top",
+        "Press the red button",
+        "Press the red button on the yellow box",
+        "Push the button downward",
+    ],
+    "coffee-pull-v2":[
+        "Pull the coffee cup from the coffee machine",
+        "Extract the coffee cup",
+        "Remove the coffee cup from the coffee machine",
+        "Drag the coffee cup",
+    ],
+    "dial-turn-v2":[
+        "Turn the dial counterclockwise",
+        "Rotate the dial to the left",
+        "Twist the dial",
+        "Adjust the dial counterclockwise"
+    ],
+    "door-open-v2":[
+        "Rotate the door counterclockwise",
+        "Swing the door open",
+        "Draw the door open",
+        "Draw the door counterclockwise"
+    ],
+    "door-unlock-v2":[
+        "Turn door lock clockwise",
+        "Rotate the door lock",
+        "Twist the door lock",
+        "Adjust the door lock"
+    ],
+    "drawer-close-v2":[
+        "Push the drawer closed",
+        "Shut the drawer",
+        "Slide the drawer shut",
+        "Close the green drawer"
+    ],
+    "faucet-open-v2":[
+        "Rotate the faucet clockwise",
+        "Twist the faucet in a clockwise direction",
+        "Turn the faucet handle to the left",
+        "Adjust the faucet clockwise"
+    ],
+    "handle-press-v2":[
+        "Press the handle downward",
+        "Depress the handle",
+        "Lower the handle",
+        "Apply downward pressure on the handle"
+        ],
+    "handle-pull-side-v2":[
+        "Pull the handle upward",
+        "Lift the handle",
+        "Raise the handle",
+        "Apply upward force to the handle"
+    ],
+    "peg-insert-side-v2":[
+        "Push the peg into the hole",
+        "Move the peg into the box",
+        "Insert the peg into the hole",
+        "Guide the peg into the hole"
+    ],
+    "pick-place-v2":[
+        "Lift the block to the goal position",
+        "Pick up the block and setting it at the goal position",
+        "Grab the block and positioning it at the goal",
+        "Move the box to the goal position"
+    ],
+    "plate-slide-v2":[
+        "Push the plate into the gate",
+        "Glide the plate into the gate",
+        "Move the plate into the gate",
+        "Shift the plate into the gate"
+    ],
+    "plate-slide-back-side-v2":[
+        "Pull the plate out of the gate",
+        "Glide the plate out of the gate",
+        "Move the plate out of the gate",
+        "Shift the plate out of the gate"
+    ],
+    "push-v2":[
+        "Move the block to the goal",
+        "Slide the block toward the goal",
+        "Propel the block to the goal",
+        "Drive the block toward the goal"
+    ],
+    "reach-v2":[
+        "Extend to the goal",
+        "Stretch toward the goal",
+        "Move to the goal",
+        "Direct toward the goal"
+    ],
+    "stick-push-v2":[
+        "Move the stick",
+        "Slide the stick",
+        "Shift the stick",
+        "Propel the stick"
+    ],
+    "stick-pull-v2":[
+        "Pull the stick",
+        "Extract the stick",
+        "Move the stick to the bottle",
+        "Drag the stick"
+    ],
+    "window-open-v2":[
+        "Pull the window open",
+        "Slide the window open",
+        "Move the window open",
+        "Shift the window open"
+    ],
+    "hand-insert-v2":[
+        "Move up the block and put it into the hole",
+        "Insert the block into the hole",
+        "Place the block into the hole",
+        "Guide the block into the hole"
+    ]
+}
+
+
+
+
+
