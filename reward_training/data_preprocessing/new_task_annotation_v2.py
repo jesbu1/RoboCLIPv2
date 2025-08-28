dc58f16fd9c699d5c837d79b528aaeae3d782054
jiahui <jzhang96@usc.edu>
2025-08-28 13:34:29 -0700

    update ann

---
+++ reward_training/data_preprocessing/new_task_annotation_v2.py
@@ -0,0 +1,172 @@
+TRAIN_GT_ANN = {
+        "button-press-v2": "Pressing the button from side",
+        "button-press-topdown-wall-v2": "Pressing the button from top",
+        "coffee-pull-v2": "Pulling the coffee cup",
+        "dial-turn-v2": "Turning the dial",
+        "door-open-v2": "Opening the door",
+        "door-unlock-v2": "Turning door lock clockwise",
+        "drawer-close-v2": "Closing the drawer",
+        "faucet-open-v2": "Opening the faucet",
+        "handle-press-v2": "Pressing the handle",
+        "handle-pull-side-v2": "Pulling the handle up from the side",
+        "peg-insert-side-v2": "Inserting the peg from the side",
+        "pick-place-v2": "Picking up the block and placing it to the goal position",
+        "plate-slide-v2": "Sliding the plate into the gate",
+        "plate-slide-back-side-v2": "Sliding the plate out of the gate from the side",
+        "push-v2": "Pushing the block to the goal",
+        "reach-v2": "Reaching the goal",
+        "stick-push-v2": "Pushing the stick",
+        "stick-pull-v2": "Pulling the stick",
+        "window-open-v2": "Opening the window",
+        "hand-insert-v2": "Picking up the block and insert it into the hole",
+}
+
+EVAL_GT_ANN = {
+        "button-press-wall-v2": "Pressing the button from side",
+        "button-press-topdown-v2": "Pressing the button from top",
+        "coffee-push-v2": "Pushing the coffee cup",
+        "coffee-button-v2": "Pressing the coffee button",
+        "door-close-v2": "Cloing the door",
+        "door-lock-v2": "Turning door lock counter-clockwise",
+        # "drawer-open-v2": "Opening the drawer",
+        "faucet-close-v2": "Closing the faucet",
+        "handle-press-side-v2": "Pressing the handle from side",
+        "handle-pull-v2": "Pulling the handle",
+        # "peg-unplug-v2": "Pulling the peg",
+        "pick-place-wall-v2": "Picking up the block and placing it to the goal position",
+        "plate-slide-back-v2": "Sliding the plate out of the gate",
+        "plate-slide-side-v2": "Sliding the plate into the gate from the side",
+        "push-back-v2": "Pushing the block back to the goal",
+        "reach-wall-v2": "Reaching the goal",
+        "soccer-v2": "Sliding the ball into the gate",
+        "window-close-v2": "Closing the window",
+        "sweep-into-v2": "Sweeping the block into the hole",
+}
+
+GENERATE_TRAIN_ANN = {
+    "button-press-v2":[
+        "Pressing the button from the side",
+        "Pressing the red button",
+        "Pressing the button on the yellow box",
+        "Pushing the button inside"
+    ],
+    "button-press-topdown-wall-v2":[
+        "Pressing the button from top",
+        "Pressing the red button",
+        "Pressing the red button on the yellow box",
+        "Pushing the button downward",
+    ],
+    "coffee-pull-v2":[
+        "Pulling the coffee cup from the coffee machine",
+        "Extracting the coffee cup",
+        "Removing the coffee cup from the coffee machine",
+        "Dragging the coffee cup",
+    ],
+    "dial-turn-v2":[
+        "Turning the dial counterclockwise",
+        "Rotating the dial to the left",
+        "Twisting the dial",
+        "Adjusting the dial counterclockwise"
+    ],
+    "door-open-v2":[
+        "Rotating the door counterclockwise",
+        "Swinging the door open",
+        "Drawing the door open",
+        "Drawing the door counterclockwise"
+    ],
+    "door-unlock-v2":[
+        "Turning door lock clockwise",
+        "Rotating the door lock",
+        "Twisting the door lock",
+        "Adjusting the door lock"
+    ],
+    "drawer-close-v2":[
+        "Pushing the drawer closed",
+        "Shutting the drawer",
+        "Sliding the drawer shut",
+        "Closing the green drawer"
+    ],
+    "faucet-open-v2":[
+        "Rotating the faucet clockwise",
+        "Twisting the faucet in a clockwise direction",
+        "Turning the faucet handle to the left",
+        "Adjusting the faucet clockwise"
+    ],
+    "handle-press-v2":[
+        "Pressing the handle downward",
+        "Depressing the handle",
+        "Lowering the handle",
+        "Applying downward pressure on the handle"
+        ],
+    "handle-pull-side-v2":[
+        "Pulling the handle upward",
+        "Lifting the handle",
+        "Raising the handle",
+        "Applying upward force to the handle"
+    ],
+    "peg-insert-side-v2":[
+        "Pushing the peg into the hole from the side",
+        "Moving the peg into the box",
+        "Inserting the peg into the hole",
+        "Guiding the peg into the hole"
+    ],
+    "pick-place-v2":[
+        "Lifting the block to the goal position",
+        "Picking up the block and setting it at the goal position",
+        "Grabbing the block and positioning it at the goal",
+        "Moving the box to the goal position"
+    ],
+    "plate-slide-v2":[
+        "Pushing the plate into the gate",
+        "Gliding the plate into the gate",
+        "Moving the plate into the gate",
+        "Shifting the plate into the gate"
+    ],
+    "plate-slide-back-side-v2":[
+        "Pulling the plate out of the gate",
+        "Gliding the plate out of the gate",
+        "Moving the plate out of the gate",
+        "Shifting the plate out of the gate"
+    ],
+    "push-v2":[
+        "Moving the block to the goal",
+        "Sliding the block toward the goal",
+        "Propelling the block to the goal",
+        "Driving the block toward the goal"
+    ],
+    "reach-v2":[
+        "Extending to the goal",
+        "Stretching toward the goal",
+        "Moving to the goal",
+        "Directing toward the goal"
+    ],
+    "stick-push-v2":[
+        "Moving the stick",
+        "Sliding the stick",
+        "Shift the stick",
+        "Propelling the stick"
+    ],
+    "stick-pull-v2":[
+        "Pulling the stick",
+        "Extracting the stick",
+        "Moving the stick to the bottle",
+        "Dragging the stick"
+    ],
+    "window-open-v2":[
+        "Pulling the window open",
+        "Sliding the window open",
+        "Moving the window open",
+        "Shifting the window open"
+    ],
+    "hand-insert-v2":[
+        "Moving up the block and put it into the hole",
+        "Inserting the block into the hole",
+        "Placing the block into the hole",
+        "Guiding the block into the hole"
+    ]
+}
+
+
+
+
+
